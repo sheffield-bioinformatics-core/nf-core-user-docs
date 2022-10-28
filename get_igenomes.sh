1764d8b2b1d2e0cc209454766e50b22bd10bfb14
Mark Dunning <md1mjdx@sharc-login1.shef.ac.uk>
2022-10-28 11:34:57 +0100

    add code to download zebrafish and fly genomes

--- get_igenomes.sh
+++ get_igenomes.sh
@@ -65,12 +65,59 @@ chmod +x aws-igenomes.sh
                   -b GRCm38 \
                   -t fasta
 
-./aws-igenomes.sh -g Mus_musculus \
+
+## Zebrafish
+
+./aws-igenomes.sh -g Danio_rerio \
                   -s NCBI \
-                  -b GRCm38 \
+                  -b GRCz10 \
                   -t gtf
 
+./aws-igenomes.sh -g Danio_rerio \
+                  -s NCBI \
+                  -b GRCz10 \
+                  -t bed12
+
+./aws-igenomes.sh -g Danio_rerio \
+                  -s NCBI \
+                  -b GRCz10 \
+                  -t bwa
+
+./aws-igenomes.sh -g Danio_rerio \
+                  -s NCBI \
+                  -b GRCz10 \
+                  -t star
+
+./aws-igenomes.sh -g Danio_rerio \
+                  -s NCBI \
+                  -b GRCz10 \
+                  -t fasta
 
 
+## Fly
 
+./aws-igenomes.sh -g Drosophila_melanogaster \
+                  -s NCBI \
+                  -b build5.41 \
+                  -t gtf
+
+./aws-igenomes.sh -g Drosophila_melanogaster \
+                  -s NCBI \
+                  -b build5.41 \
+		  -t bed12
+
+./aws-igenomes.sh -g Drosophila_melanogaster \
+                  -s NCBI \
+                  -b build5.41 \
+		  -t bwa
+
+./aws-igenomes.sh -g Drosophila_melanogaster \
+                  -s NCBI \
+                  -b build5.41 \
+		  -t star
+
+./aws-igenomes.sh -g Drosophila_melanogaster \
+                  -s NCBI \
+                  -b build5.41 \
+		  -t fasta
 
